f940169b9480701190e6bc8cc498410f1a4812b9
Matthijs Knigge <matthijs@volcano.nl>
2024-04-25 18:28:03 +0200

    feat(game): Add game state and stats

--- src/game/index.ts
+++ src/game/index.ts
@@ -2,16 +2,26 @@ import logger from "$utils/logger";
 import BasicBrain from "src/brains/basic";
 import Player from "./objects/player";
 import type { BrainConstructor, brainGameData } from "./objects/brain";
+import { CellState } from "./objects/playboardCell";
+import { ShipState } from "./objects/ship";
 
 export type GameArgs = {
     player1Brain: BrainConstructor,
-    player2Brain: BrainConstructor
+    player2Brain: BrainConstructor,
+    renderSettings?: RenderSettings
+}
+
+type RenderSettings = {
+    fullGame: boolean
 }
 
 export default class Game {
     player1: Player;
     player2: Player;
     playerTurn: 1 | 2 = 1;
+    winner: Player | false = false;
+    renderSettings: RenderSettings;
+    turnCount: number = 0;
 
     constructor(args: GameArgs){
         this.player1 = new Player(
@@ -23,23 +33,27 @@ export default class Game {
             args.player2Brain
         );
 
-        this.render();
-        this.start();
+        this.renderSettings = args.renderSettings || {
+            fullGame: true
+        };
 
-        setInterval(() => {
-            this.updateTick();
-        }, 100);
+        this.render();
     }
 
     start(){
         this.player1.start();
         this.player2.start();
 
-        // lock the boards
         this.player1.playboard.lock()
         this.player2.playboard.lock();
 
         this.render();
+
+        while(!this.winner){
+            this.updateTick();
+            this.checkForGameOver();
+            this.render();
+        }
     }
 
     updateTick(){
@@ -60,8 +74,18 @@ export default class Game {
         this.updatePlayerData();
 
         // finish the turn
-        this.render();
         this.playerTurn = this.playerTurn === 1 ? 2 : 1;
+        this.turnCount++;
+    }
+
+    checkForGameOver(){
+        if (this.player1.allShipsSunk()){
+            this.winner = this.player2;
+            logger.log("Player 2 wins!");
+        } else if (this.player2.allShipsSunk()){
+            this.winner = this.player1;
+            logger.log("Player 1 wins!");
+        }
     }
 
     updatePlayerData(){
@@ -81,6 +105,9 @@ export default class Game {
     }
 
     render(){
+        if (!this.renderSettings.fullGame){
+            return;
+        }
         // clear the screen
         console.clear();
 
@@ -99,5 +126,37 @@ export default class Game {
         console.log("");
         console.log("");
         logger.printAll(20);
+    }
+
+    getStats(){
+        if (!this.isGameOver()){
+            logger.error("Game is not over. Cannot get stats");
+            return false;
+        }
+        const player1TotalHits = this.player1.playboard.getCellsByState(CellState.hit).length;
+        const player1TotalMisses = this.player1.playboard.getCellsByState(CellState.miss).length;
+        const player2TotalHits = this.player2.playboard.getCellsByState(CellState.hit).length;
+        const player2TotalMisses = this.player2.playboard.getCellsByState(CellState.miss).length;
+
+        return {
+            totalTurns: this.turnCount,
+            winner: this.winner ? this.winner.name : false,
+            player1: {
+                hitsTaken: player1TotalHits,
+                accuracy: Math.round(
+                    100 / (player2TotalHits + player2TotalMisses) * player2TotalHits * 100
+                ) / 100,
+            },
+            player2: {
+                hitsTaken: player2TotalHits,
+                accuracy: Math.round(
+                    100 / (player1TotalHits + player1TotalMisses) * player1TotalHits * 100
+                ) / 100,
+            }
+        }
+    }
+
+    isGameOver(){
+        return this.winner !== false;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,13 @@ import BasicBrain from "./brains/basic";
 const game = new Game({
     player1Brain: BasicBrain,
     player2Brain: BasicBrain
-});
+    renderSettings: {
+        fullGame: true
+    }
+})
+
+game.start();
+
+const stats = game.getStats()
+
+console.log(stats);
