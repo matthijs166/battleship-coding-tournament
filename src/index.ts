324a93d2224e61aef5bdc915889a1b4e5b86226d
Matthijs Knigge <matthijs@volcano.nl>
2024-06-23 15:23:53 +0200

    feat(cli): Init

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,81 @@
+import { parseArgs } from "util";
+import Benchmark from "./benchmarking";
 import Game from "./game";
-import BasicBrain from "./brains/basic";
-import BasicOptimized from "./brains/basicOptimized";
+import brainLoader from "$utils/brainLoader";
 
-const game = new Game({
-    player1Brain: BasicBrain,
-    player2Brain: BasicOptimized,
-    renderSettings: {
-        fullGame: true
-    }
-})
 
-game.start();
+let args = parseArgs({
+    args: Bun.argv,
+    tokens: true,
+    options: {
+        benchmark: {
+            type: 'boolean',
+        },
+        run: {
+            type: 'boolean',
+        },
+        brain: {
+            type: 'string',
+            multiple: true,
+        },
+        threads: {
+            type: 'string',
+            default: '4',
+        },
+        iterations: {
+            type: 'string',
+            default: '100',
+        },
+        simulationSpeed: {
+            type: 'string',
+            default: '-1',
+        },
+    },
+    strict: true,
+    allowPositionals: true,
+}).values
 
-const stats = game.getStats()
+// Parse brains with , | or ; separators
+args.brain = args.brain?.flatMap((brain: string) => brain.split(/[,|;]/))
 
-console.log(stats);
+if (!args.brain?.length || args.brain?.length === 0) {
+    console.error("You need to provide at least one brain")
+    process.exit(1)
+}
+
+if (args.brain?.length === 1) {
+    console.log("We duplicate the brain for the second player")
+    args.brain.push(args.brain[0])
+}
+
+// Run benchmarking or game
+if (args.benchmark) {
+    runBenchmark()
+}
+else if (args.run) {
+    runGame()
+}
+
+async function runBenchmark() {
+    new Benchmark({
+        iterations: 1000,
+        threads: 10,
+        brainFileNames: args.brain!
+    })
+}
+
+async function runGame() {
+    console.log("Running game");
+    
+    const game = new Game({
+        player1Brain: await brainLoader(args.brain![0]),
+        player2Brain: await brainLoader(args.brain![1]),
+        renderSettings: {
+            fullGame: true
+        },
+        simulationSpeed: parseInt(args.simulationSpeed ?? "-1")
+    })
+    game.start();
+    const stats = game.getStats()
+    console.log(stats);
+}
